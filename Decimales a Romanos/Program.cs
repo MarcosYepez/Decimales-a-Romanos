50c21727b511ce0a62e51875b9264cd5c462a9b8
MARCOS MIGUEL YEPEZ URBAEZ <1090192@est.intec.edu.do>
2022-05-18 02:30:52 -0400

    Primer Commit

--- Decimales a Romanos/Program.cs
+++ Decimales a Romanos/Program.cs
@@ -6,10 +6,39 @@ using System.Threading.Tasks;
 
 namespace Decimales_a_Romanos
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            int n;
+            n = 2365;
+            Console.WriteLine("Valor Entero Principal: " + n);
+            Console.WriteLine("De tal valor Entero de Numero Romanos: ");
+            Console.WriteLine(int_to_Roman(n));
+            n = 254;
+            Console.WriteLine("Valor Entero Principal: " + n);
+            Console.WriteLine("De tal valor Entero de Numero Romanos: ");
+            Console.WriteLine(int_to_Roman(n));
+            n = 45;
+            Console.WriteLine("Valor Entero Principal: " + n);
+            Console.WriteLine("De tal valor Entero de Numero Romanos: ");
+            Console.WriteLine(int_to_Roman(n));
+            n = 8;
+            Console.WriteLine("Valor Entero Principal: " + n);
+            Console.WriteLine("De tal valor Entero de Numero Romanos: ");
+            Console.WriteLine(int_to_Roman(n));
+        }
+
+        public static string int_to_Roman(int n)
+        {
+            string[] roman_symbol = { "MMM", "MM", "M", "CM", "DCCC", "DCC", "DC", "D", "CD", "CCC", "CC", "C", "XC", "LXXX", "LXX", "LX", "L", "XL", "XXX", "XX", "X", "IX", "VIII", "VII", "VI", "V", "IV", "III", "II", "I" };
+            int[] int_value = { 3000, 2000, 1000, 900, 800, 700, 600, 500, 400, 300, 200, 100, 90, 80, 70, 60, 50, 40, 30, 20, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+
+            var Numeros_Romanos = new StringBuilder();
+            var Indice_Numerico = 0;
+            while (n ! = 0)
+
         }
     }
 }
+
