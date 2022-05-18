76c13b24559f696e99837d29f538d481b03d3a84
MARCOS MIGUEL YEPEZ URBAEZ <1090192@est.intec.edu.do>
2022-05-18 02:48:03 -0400

    Primer Commit

--- Decimales a Romanos/Program.cs
+++ Decimales a Romanos/Program.cs
@@ -34,10 +34,10 @@ namespace Decimales_a_Romanos
             string[] roman_symbol = { "MMM", "MM", "M", "CM", "DCCC", "DCC", "DC", "D", "CD", "CCC", "CC", "C", "XC", "LXXX", "LXX", "LX", "L", "XL", "XXX", "XX", "X", "IX", "VIII", "VII", "VI", "V", "IV", "III", "II", "I" };
             int[] int_value = { 3000, 2000, 1000, 900, 800, 700, 600, 500, 400, 300, 200, 100, 90, 80, 70, 60, 50, 40, 30, 20, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
 
-            var Numeros_Romanos = new StringBuilder();
-            var Indice_Numerico = 0;
-            while (n ! = 0)
-
+            var roman_numerals = new StringBuilder();
+            var index_num = 0;
+            while (n != 0)
+            { }
         }
     }
 }
